0f968694d6dd723ff47a715ff7f6d48a09068f09
MikeCAT <mikecat@users.noreply.github.com>
2022-03-12 11:21:32 +0900

    add menu

--- ab_td4/ab_td4.ino
+++ ab_td4/ab_td4.ino
@@ -4,20 +4,20 @@
 Arduboy2 ab;
 Sprites sp;
 
-const PROGMEM uint8_t ledGraph[] = {
+const uint8_t ledGraph[] PROGMEM = {
   6, 8,
   0x1e, 0x21, 0x21, 0x21, 0x21, 0x1e,
   0x1e, 0x3f, 0x3f, 0x3d, 0x33, 0x1e
 };
 
-const PROGMEM uint8_t outGraph[] = {
+const uint8_t outGraph[] PROGMEM = {
   11, 8,
   0x3e, 0x41, 0x3e, 0x00,
   0x3f, 0x40, 0x3f, 0x00,
   0x01, 0x7f, 0x01
 };
 
-const PROGMEM uint8_t romFont[] = {
+const uint8_t romFont[] PROGMEM = {
   3, 8,
   0x0e, 0x11, 0x0e, // 0
   0x12, 0x1f, 0x10, // 1
@@ -37,8 +37,13 @@ const PROGMEM uint8_t romFont[] = {
   0x1f, 0x05, 0x05, // F
   0x00, 0x0a, 0x00  // :
 };
-const PROGMEM uint8_t inverter[] = { 0x7f, 0x7f, 0x7f, 0x7f, 0x7f};
+const uint8_t inverter[] PROGMEM = { 0x7f, 0x7f, 0x7f, 0x7f, 0x7f};
 
+const uint8_t SCREEN_MAIN = 0;
+const uint8_t SCREEN_MENU = 1;
+uint8_t screen = SCREEN_MAIN;
+
+// main UI status
 uint8_t running = 0;
 uint8_t prescaler = 10, prescaleCount = 0;
 uint8_t editing = 0;
@@ -53,16 +58,27 @@ const uint8_t BUTTON_PRESS_B = 4;
 const uint8_t BUTTON_PRESS_BA = 8;
 uint8_t buttonStatus = BUTTON_PRESS_NONE;
 
+// CPU status
 uint8_t a = 0, b = 0, c = 0, pc = 0, out = 0, in = 0;
 uint8_t rom[16];
 
+// main UI draw status
 const uint8_t ROM_REDRAW_NONE = 0xff;
-uint8_t statusFullRedraw = 0;
+uint8_t statusFullRedraw = 1;
 uint8_t romIdxRedraw = ROM_REDRAW_NONE, romBitRedraw = ROM_REDRAW_NONE;
 // drawn values
 uint8_t ad = 0, bd = 0, cd = 0, pcd = 0, outd = 0, ind = 0;
 uint8_t runningd = 0, editingd = 0, buttonStatusd = 0;
 uint8_t editxd = 0, edityd = 0, editscd = 0;
+
+// menu status
+const uint8_t MENU_CPU = 0;
+uint8_t menuPage = 0;
+uint8_t menuSelect = 0;
+uint8_t helpPage = 0;
+
+// menu draw status
+uint8_t menuRedraw = 0;
 
 void emulateOneCycle() {
   pc = (pc + 1) & 0xf;
@@ -100,6 +116,8 @@ void updateMainUI(uint8_t releasedButton) {
       break;
     case BUTTON_PRESS_BA:
       running = 0;
+      screen = SCREEN_MENU;
+      menuRedraw = 1;
       break;
   }
   if (editing) {
@@ -292,11 +310,223 @@ void drawMainUI() {
   }
 }
 
+void updateMenu() {
+  if (ab.justReleased(RIGHT_BUTTON)) {
+    menuPage++;
+    if (menuPage > 3) menuPage = 0;
+    if (menuPage == 2 && menuSelect > 2) menuSelect = 2;
+    menuRedraw = 1;
+  }
+  if (ab.justReleased(LEFT_BUTTON)) {
+    if (menuPage == 0) menuPage = 3; else menuPage--;
+    if (menuPage == 2 && menuSelect > 2) menuSelect = 2;
+    menuRedraw = 1;
+  }
+  if (ab.justReleased(DOWN_BUTTON)) {
+    if (menuPage == 3) {
+      helpPage++;
+      if (helpPage > 1) helpPage = 0;
+    } else {
+      menuSelect++;
+      if (menuSelect > (menuPage == 2 ? 2 : 4)) menuSelect = 0;
+    }
+    menuRedraw = 1;
+  }
+  if (ab.justReleased(UP_BUTTON)) {
+    if (menuPage == 3) {
+      if (helpPage == 0) helpPage = 1; else helpPage--;
+    } else {
+      if (menuSelect == 0) menuSelect = (menuPage == 2 ? 2 : 4);
+      else menuSelect--;
+    }
+    menuRedraw = 1;
+  }
+  if (ab.justReleased(B_BUTTON)) {
+    screen = SCREEN_MAIN;
+    statusFullRedraw = 1;
+  }
+}
+
+void drawMenu() {
+  if (!menuRedraw) return;
+  ab.clear();
+
+  // draw menu tabs
+  const int MENU_OFFSET = 8;
+  int subMenuTextNum = 0;
+  if (menuPage == 0) {
+    subMenuTextNum = 5;
+    ab.fillRect(MENU_OFFSET + 0, 0, 6 * 3 + 1, 9, WHITE);
+    ab.setTextColor(BLACK);
+  } else {
+    ab.setTextColor(WHITE);
+  }
+  ab.setCursor(MENU_OFFSET + 1, 1);
+  ab.print(F("CPU"));
+
+  if (menuPage == 1) {
+    subMenuTextNum = 6;
+    ab.fillRect(MENU_OFFSET + 24, 0, 6 * 3 + 1, 9, WHITE);
+    ab.setTextColor(BLACK);
+  } else {
+    ab.setTextColor(WHITE);
+  }
+  ab.setCursor(MENU_OFFSET + 25, 1);
+  ab.print(F("ROM"));
+
+  if (menuPage == 2) {
+    subMenuTextNum = 10;
+    ab.fillRect(MENU_OFFSET + 48, 0, 6 * 6 + 1, 9, WHITE);
+    ab.setTextColor(BLACK);
+  } else {
+    ab.setTextColor(WHITE);
+  }
+  ab.setCursor(MENU_OFFSET + 49, 1);
+  ab.print(F("EEPROM"));
+
+  if (menuPage == 3) {
+    ab.fillRect(MENU_OFFSET + 90, 0, 6 * 4 + 1, 9, WHITE);
+    ab.setTextColor(BLACK);
+  } else {
+    ab.setTextColor(WHITE);
+  }
+  ab.setCursor(MENU_OFFSET + 91, 1);
+  ab.print(F("HELP"));
+
+  ab.drawFastHLine(0, 9, 128, WHITE);
+
+  // draw sub menus
+  const int SUBMENU_Y = 11;
+  if (menuPage != 3) {
+    if (menuSelect == 0) {
+      ab.fillRect(0, SUBMENU_Y, 6 * subMenuTextNum + 1, 9, WHITE);
+      ab.setTextColor(BLACK);
+    } else {
+      ab.setTextColor(WHITE);
+    }
+    ab.setCursor(1, SUBMENU_Y + 1);
+    if (menuPage == 0) ab.print(F("RESET"));
+    else if (menuPage == 1) ab.print(F("CLEAR"));
+    else if (menuPage == 2) ab.print(F("ERASE  ALL"));
+  
+    if (menuSelect == 1) {
+      ab.fillRect(0, SUBMENU_Y + 9 * 1, 6 * subMenuTextNum + 1, 9, WHITE);
+      ab.setTextColor(BLACK);
+    } else {
+      ab.setTextColor(WHITE);
+    }
+    ab.setCursor(1, SUBMENU_Y + 1 + 9 * 1);
+    if (menuPage == 0) ab.print(F("SPEED"));
+    else if (menuPage == 1) ab.print(F("LOAD"));
+    else if (menuPage == 2) ab.print(F("IMPORT ALL"));
+  
+    if (menuSelect == 2) {
+      ab.fillRect(0, SUBMENU_Y + 9 * 2, 6 * subMenuTextNum + 1, 9, WHITE);
+      ab.setTextColor(BLACK);
+    } else {
+      ab.setTextColor(WHITE);
+    }
+    ab.setCursor(1, SUBMENU_Y + 1 + 9 * 2);
+    if (menuPage == 0) ab.print(F("INPUT"));
+    else if (menuPage == 1) ab.print(F("SAVE"));
+    else if (menuPage == 2) ab.print(F("EXPORT ALL"));
+  
+    if (menuSelect == 3) {
+      ab.fillRect(0, SUBMENU_Y + 9 * 3, 6 * subMenuTextNum + 1, 9, WHITE);
+      ab.setTextColor(BLACK);
+    } else {
+      ab.setTextColor(WHITE);
+    }
+    ab.setCursor(1, SUBMENU_Y + 1 + 9 * 3);
+    if (menuPage == 0) ab.print(F("SOUND"));
+    else if (menuPage == 1) ab.print(F("IMPORT"));
+  
+    if (menuSelect == 4) {
+      ab.fillRect(0, SUBMENU_Y + 9 * 4, 6 * subMenuTextNum + 1, 9, WHITE);
+      ab.setTextColor(BLACK);
+    } else {
+      ab.setTextColor(WHITE);
+    }
+    ab.setCursor(1, SUBMENU_Y + 1 + 9 * 4);
+    if (menuPage == 0) ab.print(F("TYPE"));
+    else if (menuPage == 1) ab.print(F("EXPORT"));
+  }
+
+  ab.setTextColor(WHITE);
+  if (menuPage == 2) {
+    ab.setCursor(1, SUBMENU_Y + 1 + 9 * 3);
+    ab.print(F("FREE"));
+    ab.setCursor(1, SUBMENU_Y + 1 + 9 * 4);
+    ab.print(F("USED"));
+    ab.setCursor(1, SUBMENU_Y + 1 + 9 * 5);
+    ab.print(F("RESERVED"));
+  } else if (menuPage == 3) {
+    if (helpPage == 0) {
+      ab.setCursor(1, SUBMENU_Y + 9 * 0);
+      ab.print(F("0011 mmmm  MOV A, Im"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 1);
+      ab.print(F("0111 mmmm  MOV B, Im"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 2);
+      ab.print(F("0001 0000  MOV A, B"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 3);
+      ab.print(F("0100 0000  MOV B, A"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 4);
+      ab.print(F("0000 mmmm  ADD A, Im"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 5);
+      ab.print(F("0101 mmmm  ADD B, Im"));
+    } else if (helpPage == 1) {
+      ab.setCursor(1, SUBMENU_Y + 9 * 0);
+      ab.print(F("0010 0000  IN  A"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 1);
+      ab.print(F("0110 0000  IN  B"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 2);
+      ab.print(F("1011 mmmm  OUT Im"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 3);
+      ab.print(F("1001 0000  OUT B"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 4);
+      ab.print(F("1111 mmmm  JMP Im"));
+      ab.setCursor(1, SUBMENU_Y + 9 * 5);
+      ab.print(F("1110 mmmm  JNC Im"));
+    }
+  }
+
+  // draw menu data
+  if (menuPage == 0) {
+    const int MENU_DATA_X = 6 * 8;
+    ab.setCursor(MENU_DATA_X, SUBMENU_Y + 1 + 9 * 1);
+    ab.print(F("10Hz"));
+    ab.setCursor(MENU_DATA_X, SUBMENU_Y + 1 + 9 * 2);
+    ab.print(F("ON/OFF"));
+    ab.setCursor(MENU_DATA_X, SUBMENU_Y + 1 + 9 * 3);
+    ab.print(F("OFF"));
+    ab.setCursor(MENU_DATA_X, SUBMENU_Y + 1 + 9 * 4);
+    ab.print(F("TD4"));
+  } else if (menuPage == 1 && (menuSelect == 1 || menuSelect == 2)) {
+    const int MENU_DATA_X = 6 * 7 + 4;
+    for (int i = 0; i < 5; i++) {
+      ab.setCursor(MENU_DATA_X, SUBMENU_Y + 1 + 9 * i);
+      ab.print(F("999. "));
+      ab.print(F("ABCDEFGH"));
+    }
+    ab.setCursor(MENU_DATA_X + 12, SUBMENU_Y + 1 + 9 * 5);
+    ab.print(F("999 / 999"));
+  } else if (menuPage == 2) {
+    const int MENU_DATA_X = 6 * 10;
+    ab.setCursor(MENU_DATA_X, SUBMENU_Y + 1 + 9 * 3);
+    ab.print(F("999 BLOCKS"));
+    ab.setCursor(MENU_DATA_X, SUBMENU_Y + 1 + 9 * 4);
+    ab.print(F("999 BLOCKS"));
+    ab.setCursor(MENU_DATA_X, SUBMENU_Y + 1 + 9 * 5);
+    ab.print(F("999 BLOCKS"));
+  }
+
+  menuRedraw = 0;
+}
+
 void setup() {
   ab.begin();
   ab.setTextSize(1);
   ab.setFrameRate(100);
-  statusFullRedraw = 1;
 }
 
 void loop() {
@@ -344,8 +574,16 @@ void loop() {
       break;
   }
 
-  updateMainUI(releasedButtonStatus);
+  switch (screen) {
+    case SCREEN_MAIN:
+      updateMainUI(releasedButtonStatus);
+      drawMainUI();
+      break;
+    case SCREEN_MENU:
+      updateMenu();
+      drawMenu();
+      break;
+  }
 
-  drawMainUI();
   ab.display();
 }
